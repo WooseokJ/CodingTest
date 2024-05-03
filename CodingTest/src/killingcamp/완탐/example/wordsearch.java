55fadf9e3279b7d9882fc42cfe0e134d491a92b2
wooseok <wooseokbird.dev@gmail.com>
2024-05-03 19:31:37 +0900

    fix: 완전탐색 word_search

--- CodingTest/src/killingcamp/완탐/example/wordsearch.java
+++ CodingTest/src/killingcamp/완탐/example/wordsearch.java
@@ -1,57 +1,58 @@
 package killingcamp.완탐.example;
 
 public class wordsearch {
-    static char[][] board = {
-            {'A', 'B','C', 'E' },
-            {'S', 'F', 'C', 'S'},
-            {'A', 'D', 'E', 'E'}
-    };
-    static String word = "ABCCED";
-    static int n = board.length;
-    static int m = board[0].length;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static boolean[][] visited = new boolean[n][m];
-
+    static int[] dr = {0,0,1,-1};
+    static int[] dc = {1,-1,0,0};
     public static void main(String[] args) {
-        System.out.println(exist(board,word));
+        char[][] board = {
+                {'A', 'B', 'C', 'E'},
+                {'S','F', 'C','S'},
+                {'A', 'D', 'E', 'E'}
+        };
+        System.out.println(exist(board, "ABCCED"));
+
     }
-
     public static boolean exist(char[][] board, String word) {
-
-        for(int i = 0; i< n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(backtracking(i,j,0)){
-                    return true;
+        int n = board.length;
+        int m = board[0].length;
+        for(int r = 0; r < n ; r++) {
+            for(int c = 0; c < m; c++) {
+                if(board[r][c] == word.charAt(0)) {
+                    char temp = board[r][c];
+                    board[r][c] = ' ';
+                    if(backtrack(board, word, r, c, 0)) {
+                        return true;
+                    }
+                    board[r][c] = temp;
                 }
             }
         }
         return false;
-
-
-
     }
-
-    public static boolean backtracking(int i, int j, int w) {
-        boolean flag = false;
-        if (!visited[i][j] && board[i][j] == word.charAt(w)) {
-            if (w == word.length() -1) {
-                return true;
-            }
-            visited[i][j] = true;
-
-
-            for (int r = 0; r < 4; r++) {
-                int cur_x = dx[r];
-                int cur_y = dy[r];
-                if ((0 <= cur_x && cur_x < n) && (0 <= cur_y && cur_y < n) ) {
-                    if(backtracking(cur_x+r, cur_y + r, w+1)){
-                        flag = true;
+    public static boolean backtrack(char[][] board, String word, int r, int c, int depth) {
+        if(depth +1  == word.length()) {return true;}
+        int n = board.length;
+        int m = board[0].length;
+        for(int i = 0 ; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(isValid(nr, nc, n, m)) {
+                if(board[nr][nc]==word.charAt(depth+1)) {
+                    char temp = board[nr][nc];
+                    board[nr][nc] = ' ';
+                    if(backtrack(board, word, nr, nc, depth + 1)) {
+                        return true;
                     }
+                    board[nr][nc] = temp;
                 }
-                visited[i][j] = false;
             }
         }
-        return flag;
+        return false;
+    }
+    public static boolean isValid(int nr, int nc, int n, int m) {
+        if((0 <= nr && nr < n) && (0 <= nc && nc < m)) {
+            return true;
+        }
+        return false;
     }
 }
