886e73a423ecb33ca22eba638b619fea94c338fa
wooseok <wooseokbird.dev@gmail.com>
2024-05-03 18:18:25 +0900

    fix: 완전탐색 binary search

--- CodingTest/src/killingcamp/완탐/example/binsearch.java
+++ CodingTest/src/killingcamp/완탐/example/binsearch.java
@@ -6,35 +6,31 @@ import java.util.List;
 
 public class binsearch {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>();
-        nums.add(32);
-        nums.add(93);
-        nums.add(-2);
-        nums.add(0);
-        nums.add(84);
-        nums.add(2023);
-        bin_search(nums, 0);
-
-    }
-    public static void bin_search(List<Integer> nums, int target) {
-
-        Collections.sort(nums);
-
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if(target > nums.get(mid)) {
-                l = mid + 1;
-            } else if(target < nums.get(mid)) {
-                r = mid - 1;
-            } else {
-                System.out.printf("찾음");
-                return;
-
-            }
+        int[] nums = {-1,0,3,5,9,12};
+        // int[] -> List<Intger> , Collections.sort(nums) 쓰려고 이유: int[]는 안에 못넣어
+        List<Integer> arr = new ArrayList<>();
+        for (int num: nums) {
+            arr.add(num);
         }
 
-        System.out.println("target이 nums에 없다.");
+        System.out.println(binary_search(arr, 9));
 
     }
+    public static int binary_search(List<Integer> nums, int target) {
+        Collections.sort(nums);
+        int l = 0, r = nums.size() - 1;
+        while(l <= r) {
+            int mid = (l+r) / 2;
+            if(nums.get(mid) < target) {
+                l = mid - 1;
+            }else if(nums.get(mid) > target) {
+                r = mid - 1;
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
 }
