4933346644a580a30751b672f8a973243ffcda47
wooseok <wooseokbird.dev@gmail.com>
2024-05-07 14:55:15 +0900

    fix: 완전탐색 permutations n개중 r개 뽑음

--- CodingTest/src/killingcamp/완탐/example/permutations.java
+++ CodingTest/src/killingcamp/완탐/example/permutations.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 public class permutations {
     public static void main(String[] args) {
         int[] nums ={1,2,3};
-        System.out.println(permute(nums));
+//        System.out.println(permute(nums));
+        System.out.println(permute2(nums, 2));
 
     }
     public static List<List<Integer>> permute(int[] nums) {
@@ -29,6 +30,28 @@ public class permutations {
                 curr.remove(curr.size() - 1);
             }
 
+        }
+    }
+
+
+
+    // n개중 r개 뽑음(n!=r)
+    public static List<List<Integer>> permute2(int[] nums, int r){
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack2(new ArrayList<>(), nums,r, ans);
+        return ans;
+    }
+    public static void backtrack2(List<Integer> curr, int[] nums, int r, List<List<Integer>> ans) {
+        if(curr.size() == r) {
+            ans.add(new ArrayList<>(curr));
+            return ;
+        }
+        for (int num : nums) {
+            if(!curr.contains(num)) {
+                curr.add(num);
+                backtrack2(curr, nums, r, ans);
+                curr.remove(curr.size()-1);
+            }
         }
     }
 }
